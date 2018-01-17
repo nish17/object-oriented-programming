44138408ce1e5d1525ded8bc952f256e14a1153d
Nimesh <nimesh3536@gmail.com>
2018-01-18 02:43:22 +0530

    works perfect

--- assign1/b1.cpp
+++ assign1/b1.cpp
@@ -30,21 +30,31 @@ class pcb{
     * prints all the data to the console.
     */
     void show(){
-      cout << "No of ICs in PCB " << noOfICs << endl;
-      cout << "perimeter of IC" << perimeter() << endl;
-      cout << "Area of IC" << area() << endl;
+      cout << "No of ICs in PCB: " << noOfICs << endl;
+      cout << "\nperimeter of IC: " << perimeter() << endl;
+      cout << "\nArea of IC: " << area() << endl;
     }
-
+    /*
+    * returns true if both objects (ICs) have same area 
+    */
+    int sameArea(pcb IC1, pcb IC2){
+      if(IC1.area() == IC2.area()) return 1;
+      else return 0;
+    }
 };
 
 int main(){
-  pcb IC1, IC2;
+  pcb IC1, IC2, IC3;
 
   IC1.set(1,5,2.5);
   IC2.set(1,5,18.9);
 
   IC1.show();
   IC2.show();
+
+  if(IC3.sameArea(IC1,IC2)) cout << "\nBoth ICs have same area\n";
+  else cout << "\nDifferent area\n";
+
 
   return 0;
 }
