f3fa7832f8883eabf39c0aea9fa9f920991149ad
Nimesh <nimesh3536@gmail.com>
2018-01-20 10:52:43 +0530

    finally its working

--- assign1/b2.cpp
+++ assign1/b2.cpp
@@ -1,22 +1,34 @@
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define SIZE 6
+#define SIZE 3
 class Messenger{
   public:
     int id;
-    string name;
-    string sendMessage;
-    string receiveMessage;
-
+    string name,messages;
+    // string receiveMessage(Messenger m2, string s){
+    //   return "s";
+    // }
     void set(int n, string m){
       id = n;
       name = m;
     }
+    void sendMessage(Messenger m1, string s){
+      m1.messages = s;
+    }
 };
+int searchIndex(int x,Messenger m[]){
+  int z;
+  for(int i=0; i<SIZE; i++){
+    if(m[i].id == x) z = i;
+  }
+  return z;
+}
+
 
 int main(){
-  int x;
-  string y;
+  int x,s,d;
+  string y,m,m1;
   Messenger node[SIZE];
   for(int i=0; i< SIZE; i++){
     cout << "Enter name and ID: ";
@@ -26,6 +38,25 @@ int main(){
   cout << "\nNode details:\n";
   for(int i=0; i< SIZE; i++){
     cout << "Node name: "<<node[i].name << " ID: " <<node[i].id << endl;
+  }
+  int count = 0;
+  while(1){
+    cout << "\n\n\n" << endl;
+    cout << "Type your message: ";
+    // getline (cin, m);
+    cin >> m;
+    // gets(m);
+    if(m == "STOP" || m=="stop") exit(1);
+    cout << "Input source and destination node ID: ";
+    cin >> s >> d;
+
+    int x = searchIndex(s,node);
+    int y = searchIndex(d,node);
+    
+    // cout << "sender's index " <<x << endl;
+    // cout << "reciever's index " << y << endl;
+    cout << "Sending Message " << m << " to destination Node name " << node[y].name << " ID "<< node[y].id<< endl;
+    cout << "Received Message " << m << " from source Node name " << node[x].name << " ID "<< node[x].id << endl;
   }
   return 0;
 }
