4fa14b2c567a85a5f92b311f0da776a6eea87b6b
Nimesh <nimesh3536@gmail.com>
2018-01-28 19:17:53 +0530

    few things are working

--- assign2/p2.cpp
+++ assign2/p2.cpp
@@ -1,83 +1,111 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define SIZE 10 // Stack SIZE is limited to 10 elements
-class Stack{
+class Stack
+{
   // int n=SIZE;
-  int _array1[SIZE],_array2[2*SIZE];
+  int _array1[SIZE], _buffer[2 * SIZE];
   int topOfStack;
-  public:
-    Stack(){
-      topOfStack = -1;
-    }
-    /*
-    * push element onto the stack
-    */  
-    void push(int n){
-      if(topOfStack < SIZE) _array1[++topOfStack] = n;
-    }
-    /*
-    * Pops element from the stack
-    */
-    void pop(){
-      topOfStack--;
-    }
-    /*
+  // static int buffer[SIZE];
+
+public:
+  Stack()
+  {
+    topOfStack = -1;
+    // buffer = new int[SIZE];
+  }
+  /*
     * eliminated Repetitive code by creating subPrint function
     */
-    void subPrint(int arr[]){ 
-      for(int i=topOfStack; i>=0; i--){
-        cout << arr[i] << " ";
-      }     
-      cout << endl; 
+  void subPrint(int arr[])
+  {
+    for (int i = topOfStack; i >= 0; i--)
+    {
+      cout << arr[i] << " ";
     }
-    /*
-    * After first _array1 gets fully populated, copying all the elements
-    * to _array2 which is of double the size of _array1.
+    cout << endl;
+  }
+  ~Stack()
+  {
+    cout << "Contents of buffer\n";
+    subPrint(_buffer);
+    cout << endl;
+    cout << "buffer flushed\n";
+    exit(1);
+  }
+  /*
+    * push element onto the stack
     */
-    void resetStack(){
-      if(topOfStack == SIZE-1){
-        cout << "Stack2:: ";
-        for(int i=SIZE-1; i>=0; i--){
-          int l = 0;
-          _array2[l++] = _array1[i];
-          cout <<_array2[l-1] << " ";
-        }
-        cout << endl;
-        topOfStack = -1;
+  void push(int n)
+  {
+    if (topOfStack < SIZE)
+      _array1[++topOfStack] = n;
+  }
+  /*
+    * Pops element from the stack
+    */
+  void pop()
+  {
+    topOfStack--;
+  }
+
+  /*
+    * After first _array1 gets fully populated, copying all the elements
+    * to _buffer which is of double the size of _array1.
+    */
+  void resetStack()
+  {
+    if (topOfStack == SIZE - 1)
+    {
+      cout << "Contents of buffer:: ";
+      for (int i = SIZE - 1; i >= 0; i--)
+      {
+        int l = 0;
+        _buffer[l++] = _array1[i];
+        cout << _buffer[l - 1] << " ";
       }
-    } 
-    /*
+      cout << endl;
+      topOfStack = -1;
+    }
+  }
+  /*
     * prints in the required form
     */
-    void print(){
-      if(topOfStack < SIZE-1){
-        cout << "Stack1:: "; 
-        subPrint(_array1);        
-      }
-      else if(topOfStack > SIZE-1 && topOfStack <2*SIZE){
-        cout << "Stack1:: "; 
-        subPrint(_array2);      
-      }
-      else if(topOfStack == SIZE-1){
-        cout << "Stack1 is now full\nStack1::";
-        subPrint(_array1);
-        resetStack();
-        cout << "elements moved to stack2\n";
-        // cout << "2=>";
-        // subPrint(_array2);
-        return;
-      }
+  void print()
+  {
+    if (topOfStack < SIZE - 1)
+    {
+      cout << "Stack1:: ";
+      subPrint(_array1);
     }
-
+    else if (topOfStack > SIZE - 1 && topOfStack < 2 * SIZE)
+    {
+      cout << "Stack1:: ";
+      subPrint(_buffer);
+    }
+    else if (topOfStack == SIZE - 1)
+    {
+      cout << "Stack1 is now full\nStack1::";
+      subPrint(_array1);
+      resetStack();
+      cout << "elements moved to buffer\n";
+      // cout << "2=>";
+      // subPrint(_buffer);
+      return;
+    }
+  }
 };
 
-
-int main(){
+int main()
+{
   Stack stack;
   int t;
-  while(1){
+  while (1)
+  {
     cout << "Input: ";
     cin >> t;
+    if (t == -1)
+      stack.~Stack();
     stack.push(t);
     stack.print();
   }
