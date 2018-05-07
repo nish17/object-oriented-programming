3ab6fdaaeea23c697d41e22a542fe31d17ad9e17
Nimesh <nimesh3536@gmail.com>
2018-05-07 11:55:01 +0530

    add Q11

--- Level 2/Q11.cc
+++ Level 2/Q11.cc
@@ -23,6 +23,7 @@ public:
   }
 
   friend String operator+(String &s1, String &s2);
+  friend void operator==(String &s1, String &s2);
   friend void show(String &s);
 };
 void show(String &s)
@@ -40,13 +41,27 @@ String operator+(String &s1, String &s2)
   strcat(temp.str, s2.str);
   return temp;
 }
-
+void operator==(String &s1, String &s2)
+{
+  String temp;
+  // temp.length = s1.length + s2.length;
+  delete temp.str;
+  // temp.str = new char[temp.length + 1];
+  // strcpy(temp.str, s1.str);
+  // strcat(temp.str, s2.str);
+  if (strcmp(s1.str, s2.str) == 0)
+    cout << "both strings are equal\n";
+  else
+    cout << "Different strings\n";
+  // return temp;
+}
 int main()
 {
-  String s1("Ram"), s2("Kumar");
+  String s1("Ram"), s2("Ram");
   String s3;
   s3 = s1 + s2;
   cout << "s3 is:";
+  s1 == s2;
   show(s3);
   // getch();
   return 0;
